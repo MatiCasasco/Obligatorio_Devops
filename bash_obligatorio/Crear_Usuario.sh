b42fd6701e3fdc12a7756541c458fdca8c38c821
MV288481 <MV288481@fi365.ort.edu.uy>
2025-12-01 02:26:05 -0300

    Update de tabulaciones

--- bash_obligatorio/Crear_Usuario.sh
+++ bash_obligatorio/Crear_Usuario.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# ==========================
-# 1. VARIABLES Y PARAMETROS
-# ==========================
+# ===================================================================================
+# 1.                            VARIABLES Y PARAMETROS
+# ===================================================================================
 
 param_i=false          # Param -i: mostrar información detallada de los usuarios procesados
 param_c=false          # Param -c: indicar que se debe asignar contraseña a los usuarios
-password=""           # Contraseña que se usará si se pasa -c
-archivo=""            # Nombre del archivo de entrada con los datos de usuarios
-contador=0            # Contador de usuarios creados
-modi=0                # Contador de usuarios modificados
+password=""            # Contraseña que se usará si se pasa -c
+archivo=""             # Nombre del archivo de entrada con los datos de usuarios
+contador=0             # Contador de usuarios creados
+modi=0                 # Contador de usuarios modificados
 
-# ==========================
-# 2. FUNCIÓN AUXILIAR: mostrar_datos
-# ==========================
+# ===================================================================================
+# 2.                       FUNCIÓN AUXILIAR: mostrar_datos
+# ===================================================================================
 # Muestra en pantalla los datos de un usuario, usando valores por defecto
 # cuando algún campo viene vacío.
 
@@ -45,9 +45,9 @@ mostrar_datos() {
     echo ""
 }
 
-# ==========================
-# 3. PARSEO DE PARÁMETROS
-# ==========================
+# ===================================================================================
+# 3.                            PARSEO DE PARÁMETROS
+# ===================================================================================
 # Soporta:
 #   -i           -> mostrar info detallada
 #   -c PASSWORD  -> asignar esa contraseña a los usuarios
@@ -84,9 +84,9 @@ while [ $# -gt 0 ]; do
     shift                        # Avanza al siguiente parámetro
 done
 
-# ==========================
-# 4. VALIDACIONES DEL ARCHIVO
-# ==========================
+# ===================================================================================
+# 4.                         VALIDACIONES DEL ARCHIVO
+# ===================================================================================
 # Verifica que se haya indicado un archivo válido y legible.
 
 if [ -z "$archivo" ]; then
@@ -109,9 +109,9 @@ if [ ! -r "$archivo" ]; then
     exit 5
 fi
 
-# ==========================
-# 5. PROCESAMIENTO DEL ARCHIVO LÍNEA POR LÍNEA
-# ==========================
+# ===================================================================================
+# 5.                PROCESAMIENTO DEL ARCHIVO LÍNEA POR LÍNEA
+# ===================================================================================
 # Formato esperado por línea: usuario:comentario:home:crear:shell
 # - crear = SI -> crear usuario y garantizar home
 # - crear = NO o vacío -> crear usuario sin asegurar home (-M)
@@ -131,9 +131,9 @@ while read -r linea; do
     # Separar campos por ":"
     IFS=":" read -r usuario comentario home crear shell <<< "$linea"
     
-    # ======================
-    # 5.1. CASO: USUARIO YA EXISTE
-    # ======================
+    # ===================================================================================
+    # 5.1.                        CASO: USUARIO YA EXISTE
+    # ===================================================================================
     if grep -q "^$usuario:" /etc/passwd; then
         echo "El usuario $usuario ya EXISTE. Se modificaron las parametros adicionales."
         
@@ -162,9 +162,9 @@ while read -r linea; do
 
         modi=$((modi+1))   # Incrementa contador de usuarios modificados
 
-    # ======================
-    # 5.2. CASO: USUARIO NO EXISTE (CREACIÓN)
-    # ======================
+    # ===================================================================================
+    # 5.2.                  CASO: USUARIO NO EXISTE (CREACIÓN)
+    # ===================================================================================
     else
         home_vacio=0       # Marca si el home se generó por defecto
 
@@ -175,70 +175,69 @@ while read -r linea; do
                 echo "ATENCION: el directorio home '$home' ya está siendo usado por otro usuario. Cambia el nombre de la home." >&2
                 continue   # Salta a la siguiente línea del archivo sin crear este usuario
             fi
-	if [ -z "$home" ]; then
-        home="/home/$usuario"
-        home_vacio=1
-	fi
+	    if [ -z "$home" ]; then
+           home="/home/$usuario"
+           home_vacio=1
+	    fi
 
-            useradd -c "$comentario" -d "$home" -s "$shell" -m "$usuario"   # -m crea el home
+         useradd -c "$comentario" -d "$home" -s "$shell" -m "$usuario"   # -m crea el home
             
-            # Asignar contraseña si corresponde
-            if [ "$param_c" = true ] && [ -n "$password" ]; then
-                echo "$password" | passwd --stdin "$usuario" &>/dev/null
-            fi
+        # Asignar contraseña si corresponde
+        if [ "$param_c" = true ] && [ -n "$password" ]; then
+           echo "$password" | passwd --stdin "$usuario" &>/dev/null
+        fi
 
-            # Verificar creación exitosa consultando /etc/passwd
-            if grep -q "^$usuario:" /etc/passwd; then
-                echo "Usuario $usuario creado correctamente."
-                if [ "$param_i" = true ]; then
-                    echo -e "Usuario $usuario creado con éxito con datos indicados:"
-                    mostrar_datos "$usuario" "$comentario" "$home" "$crear" "$shell"
-                fi
-                contador=$((contador+1))   # Incrementa contador de usuarios creados
-            else
-                echo "ATENCION: el usuario $usuario no pudo ser creado"
+        # Verificar creación exitosa consultando /etc/passwd
+        if grep -q "^$usuario:" /etc/passwd; then
+            echo "Usuario $usuario creado correctamente."
+            if [ "$param_i" = true ]; then
+                echo -e "Usuario $usuario creado con éxito con datos indicados:"
+                mostrar_datos "$usuario" "$comentario" "$home" "$crear" "$shell"
             fi
+                contador=$((contador+1))   # Incrementa contador de usuarios creados
+         else
+                echo "ATENCION: el usuario $usuario no pudo ser creado"
+        fi
 
         # --- Caso crear = "NO" o vacío: crear usuario sin asegurar home ---
-        elif [ "$crear" = "NO" ] || [ -z "$crear" ]; then
-            # Si no viene home, usar /home/usuario como valor por defecto
-            if [ -z "$home" ]; then
-                home="/home/$usuario"
-                home_vacio=1              # Marca que el home fue asumido por defecto
-            fi
+    elif [ "$crear" = "NO" ] || [ -z "$crear" ]; then
+        # Si no viene home, usar /home/usuario como valor por defecto
+        if [ -z "$home" ]; then
+            home="/home/$usuario"
+            home_vacio=1              # Marca que el home fue asumido por defecto
+        fi
 
-            # Verificar que el home no esté ya siendo usado por otro usuario
-            if [ -n "$home" ] && grep -q "^[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:$home:" /etc/passwd; then
-                echo "ATENCION: el directorio home '$home' ya está siendo usado por otro usuario. Cambia el nombre de la home." >&2
-                continue   # Salta a la siguiente línea del archivo sin crear este usuario
-            fi
+        # Verificar que el home no esté ya siendo usado por otro usuario
+        if [ -n "$home" ] && grep -q "^[^:]*:[^:]*:[^:]*:[^:]*:[^:]*:$home:" /etc/passwd; then
+            echo "ATENCION: el directorio home '$home' ya está siendo usado por otro usuario. Cambia el nombre de la home." >&2
+            continue   # Salta a la siguiente línea del archivo sin crear este usuario
+        fi
 
-            useradd -c "$comentario" -d "$home" -s "$shell" -M "$usuario"   # -M no crea directorio home
+        useradd -c "$comentario" -d "$home" -s "$shell" -M "$usuario"   # -M no crea directorio home
             
-            # Asignar contraseña si corresponde
-            if [ "$param_c" = true ] && [ -n "$password" ]; then
-                echo "$password" | passwd --stdin "$usuario" &>/dev/null
-            fi
+        # Asignar contraseña si corresponde
+        if [ "$param_c" = true ] && [ -n "$password" ]; then
+            echo "$password" | passwd --stdin "$usuario" &>/dev/null
+        fi
 
-            # Verificar creación exitosa
-            if grep -q "^$usuario:" /etc/passwd; then
-                if [ "$param_i" = true ]; then
-                    echo "Usuario $usuario se ha creado correctamente."
-                    echo -e "Usuario $usuario creado con éxito con datos indicados:"
-                    mostrar_datos "$usuario" "$comentario" "$home" "$crear" "$shell" "$home_vacio"
-                fi
-                contador=$((contador+1))
-            else
-                echo "ATENCION: el usuario $usuario no pudo ser creado" >&2
+        # Verificar creación exitosa
+        if grep -q "^$usuario:" /etc/passwd; then
+            if [ "$param_i" = true ]; then
+                echo "Usuario $usuario se ha creado correctamente."
+                echo -e "Usuario $usuario creado con éxito con datos indicados:"
+                mostrar_datos "$usuario" "$comentario" "$home" "$crear" "$shell" "$home_vacio"
             fi
+            contador=$((contador+1))
+        else
+            echo "ATENCION: el usuario $usuario no pudo ser creado" >&2
         fi
     fi
     
 done < "$archivo"
 
-# ==========================
-# 6. RESÚMEN FINAL
-# ==========================
+# ===================================================================================
+# 6.                            RESÚMEN FINAL
+# ===================================================================================
 
 if [ "$modi" -ne 0 ]; then
     echo "Se han modificado $modi usuarios con exito"
